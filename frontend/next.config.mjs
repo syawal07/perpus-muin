e6898fcfb3291f7c3a7456b17d2a1db7e706ed10
syawalsss <syawal_ojt@solvera.id>
2026-06-23 07:42:18 +0700

    update commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,30 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "adminperpus.muallimin.sch.id",
-        pathname: "/storage/**",
       },
       {
         protocol: "https",
         hostname: "perpustakaan.muallimin.sch.id",
-        pathname: "/**",
       },
       {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
-        pathname: "/storage/**",
       },
       {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
-        pathname: "/storage/**",
       },
     ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 };
 
