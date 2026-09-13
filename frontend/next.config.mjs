16e160577ad364996cee313db9e09f883b57f8ab
syawalsss <syawalaja0712@gmail.com>
2026-09-13 19:33:45 +0700

    Update config dan GitHub Actions

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,37 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-
   images: {
     unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "adminperpus.muallimin.sch.id",
+        pathname: "/storage/**", // Penambahan gembok ekstra khusus ke folder gambar
       },
       {
         protocol: "https",
         hostname: "perpustakaan.muallimin.sch.id",
       },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-      },
     ],
   },
-
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
     },
   },
-
   logging: {
     fetches: {
       fullUrl: true,
